da7191a4feeb901ce6f0c4aa02d79de7b69ca5e3
deepak104080 <deepak104080@gmail.com>
2023-02-18 21:57:18 +0530

    assg

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 import { movies } from "../utils/movieList";
 
 const App = () => {
     const [inputValue, setInputValue] = useState("");
-    const [results, setResult] = useState(null);
+    const [results, setResult] = useState([]);
 
-    function handleClick(e) {}
+    function handleClick(e) {
+        e.preventDefault();
+        
+        const filteredResult = movies.filter((item) => {
+            return item.title === inputValue;
+        })
 
-    function handleChange(e) {}
+        const filterMovieList = filteredResult.map((item) => {
+            return item.title
+        })
+
+        setResult(filterMovieList);
+
+    }
+
+    function handleChange(e) {
+        setInputValue(e.target.value);
+    }
+
+    // useEffect(() => {
+    //     console.log('inputValue - ', inputValue);
+    //     console.log('results - ', results);
+    // })
 
     return (
         <div id="main">
